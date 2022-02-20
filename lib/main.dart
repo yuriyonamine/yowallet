4fd382942696104f0c5dba5a48ab5a21adc7aa51
Yuri Yonamine <infonamina@gmail.com>
2022-02-20 00:06:37 -0300

    Initial API configuration to access CoinBase Pro (before refactoring)

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,151 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Yo Wallet'),
+          ),
+          body: const MainPage()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MainPageState createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  double coinsQuantity = 0;
+  double totalInvested = 0;
+  String apiKey = "";
+  String secret = "";
+  String passphrase = "";
 
-  void _incrementCounter() {
+  // ISO 8601
+  void value() async {
+    CryptoProvider cryptoService = CryptoProvider();
+    InvestmentState? investmentState = cryptoService.getInvestimentsState();
+
+    String requestMethod = "GET";
+    String requestPath =
+        "/fills?product_id=ETH-GBP&profile_id=default&limit=100";
+    String requestBody = "";
+    int requestTimestamp =
+        DateTime.now().toUtc().millisecondsSinceEpoch ~/ 1000;
+
+    String message =
+        requestTimestamp.toString() + requestMethod + requestPath + requestBody;
+
+    List<int> apiKeyDecode = base64.decode(secret);
+    Hmac hmac = Hmac(sha256, apiKeyDecode);
+    Digest digest = hmac.convert(utf8.encode(message));
+    String signature = base64.encode(digest.bytes);
+
+    Uri url = Uri.parse('https://api.exchange.coinbase.com' + requestPath);
+    Map<String, String> headers = {
+      "CB-ACCESS-SIGN": signature,
+      "CB-ACCESS-TIMESTAMP": requestTimestamp.toString(),
+      "CB-ACCESS-KEY": apiKey,
+      'CB-ACCESS-PASSPHRASE': passphrase
+    };
+    var response = await http.get(url, headers: headers);
+
+    final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
+    var fills = parsed.map<Fill>((json) => Fill.fromJson(json)).toList();
+    double totalSize = 0;
+    double totalPrice = 0;
+    for (Fill fill in fills) {
+      totalSize += fill.size;
+      totalPrice += fill.price * fill.size;
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      coinsQuantity = totalSize;
+      totalInvested = totalPrice;
     });
+    print(totalSize);
+    print((2896.03 - totalPrice) * 100 / 2894.95);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      children: [
+        Text('Ethereum', style: TextStyle(fontSize: 20)),
+        Text('Coins quantity: $coinsQuantity', style: TextStyle(fontSize: 20)),
+        Text('Total invested: $totalInvested', style: TextStyle(fontSize: 20)),
+        Text('Current amount:', style: TextStyle(fontSize: 20)),
+        TextButton(
+          onPressed: () {
+            value();
+          },
+          child: Text('Update'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
+    );
+  }
+}
+
+class InvestmentState {
+  List<CoinState> coinState;
+
+  InvestmentState(this.coinState);
+}
+
+class CoinState {
+  String coinName;
+  double coinsQuantity;
+  double totalInvested;
+
+  CoinState(this.coinName, this.coinsQuantity, this.totalInvested);
+}
+
+class CryptoProvider {
+  InvestmentState? getInvestimentsState() {
+    return null;
+  }
+}
+
+class Fill {
+  final String productId;
+  final double price;
+  final double size;
+  final double fee;
+  final String side;
+
+  const Fill({
+    required this.productId,
+    required this.price,
+    required this.size,
+    required this.fee,
+    required this.side,
+  });
+
+  factory Fill.fromJson(Map<String, dynamic> json) {
+    return Fill(
+      productId: json['product_id'] as String,
+      price: double.parse(json['price']),
+      size: double.parse(json['size']),
+      fee: double.parse(json['fee']),
+      side: json['side'] as String,
     );
   }
 }
